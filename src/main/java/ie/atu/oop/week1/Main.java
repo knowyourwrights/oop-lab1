ce7450b4d2ddf8890c5484231591762dbae62e40
JESSEPRIETOSOLOVYOVA <g00479739@atu.ie>
2026-09-23 16:29:29 +0100

    Created a new book class & created an instance in main

--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -6,5 +6,15 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("fdgkvfbv");
+
+        Book firstBook = new Book();
+        firstBook.title = "Dune";
+        firstBook.author = "Frank Herbert";
+        firstBook.pageCount = 412;
+
+        System.out.println("This book is " + firstBook.title);
+        System.out.println("The author of this book is " + firstBook.author);
+        System.out.println("The page count of this book is " + firstBook.pageCount);
+        System.out.println("The status of this book is " + firstBook.available);
     }
 }
